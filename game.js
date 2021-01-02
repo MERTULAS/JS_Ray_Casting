fed489c702c48fba1b60a1666ff9346509dcbe6e
Mert ULAS <h.mert.ulas@gmail.com>
2021-01-02 19:41:03 +0300

    version 0.2.0
    The back of the walls have set to be invisible.

--- game.js
+++ game.js
@@ -4,7 +4,14 @@ body.appendChild(canvas);
 var ctx = canvas.getContext("2d");
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
-const wallPoints = [];
+const wallPoints = [
+    [200, 300, 200, 600],
+    [1500, 300, 1500, 600],
+    [900, 900, 500, 900],
+    [300, 300, 600, 100],
+    [200, 700, 500, 800],
+];
+const walls = [];
 window.addEventListener("resize", () => {
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
@@ -14,7 +21,13 @@ window.addEventListener("load", game);
 
 function game(){
 
-    let wall = new Wall(400, 300, 400, 600);
+    wallPoints.forEach(points => {
+        const x1 = points[0]
+        const y1 = points[1]
+        const x2 = points[2]
+        const y2 = points[3]
+        walls.push(new Wall(x1, y1, x2, y2));
+    });
 
     let player = new Player(canvas.width / 2 - 25, canvas.height / 2 - 25, 50, 50);
     
@@ -30,19 +43,20 @@ function game(){
         ctx.fillStyle = "rgba(33, 33, 33, 0.3)";
         ctx.fillRect(0, 0, canvas.width, canvas.height);
         player.draw();
-        wall.drawWall();
-        player.rayCasting(wall);
-        console.log(player.castingPoints);
+        walls.forEach(wall => {
+            wall.drawWall();
+            player.fieldOfView(wall);
+        });
+        player.rays();
+        /*
         player.castingPoints.forEach(point => {
             ctx.beginPath();
             ctx.arc(point.x, point.y, 10, 0, Math.PI * 2);
             ctx.fill();
-        })
-        player.fieldOfView();
+        })*/
+        
         requestAnimationFrame(play);
     }
     play();
     
 }
-
-
--- player.js
+++ player.js
@@ -6,9 +6,12 @@ class Player{
         this.height = height;
         this.radius = -30;
         this.viewAngle = 60;
-        this.visionDistance = 400;
-        this.rays = [];
+        this.angleStep = 1;
+        this.visionDistance = 10;
+        this.lengthPerIndexes = [...Array(this.viewAngle / this.angleStep + 1)].map((_, i) => Infinity); 
+        this.castingRays = {};
         this.castingPoints = [];
+        this.points = {};
         this.controller = {
             "87": {pressed: false, dx: 10, dy: 10, angle: 0},
             "83": {pressed: false, dx: -10, dy:-10, angle: 0},
@@ -17,52 +20,72 @@ class Player{
         };
     }
 
-    fieldOfView(){
-        /*let dx = this.visionDistance * Math.cos(this.radius * Math.PI / 180) + this.x + this.width / 2;
-        let dy = this.visionDistance * Math.sin(this.radius * Math.PI / 180) + this.y + this.height / 2;
-        let dx2 = this.visionDistance * Math.cos((this.radius + this.viewAngle) * Math.PI / 180) + this.x + this.width / 2;
-        let dy2 = this.visionDistance * Math.sin((this.radius + this.viewAngle) * Math.PI / 180) + this.y + this.width / 2;
-        */
-
-        ctx.beginPath();
-        this.rays = [];
-        for (let i = 0; i <= this.viewAngle; i+=0.5){
+    fieldOfView(wall){
+        let index = 0;
+        ctx.strokeStyle = "rgba(255, 255, 255, 0.1)";
+        for (let i = 0; i <= this.viewAngle; i+=this.angleStep){
             let dx = this.visionDistance * Math.cos((this.radius + i) * Math.PI / 180) + this.x + this.width / 2;
             let dy = this.visionDistance * Math.sin((this.radius + i) * Math.PI / 180) + this.y + this.width / 2;
-            ctx.strokeStyle = "white";
-            ctx.moveTo(this.x + this.width / 2, this.y + this.height / 2);
-            ctx.lineTo(dx, dy);
-            this.rays.push([dx, dy]);
+            
+            let rayCastingPoint = this.rayCasting(wall, dx, dy);
+            if(rayCastingPoint)
+            {
+                
+                this.castingRays[index] = this.distance(this.x + this.width / 2, this.y + this.height / 2, rayCastingPoint.x, rayCastingPoint.y);
+                
+            }
+            if((this.castingRays[index] < this.lengthPerIndexes[index])){
+                dx = rayCastingPoint.x;
+                dy = rayCastingPoint.y;
+                this.lengthPerIndexes[index] = this.castingRays[index];
+                this.points[index] = [dx, dy];
+                ctx.moveTo(this.x + this.width / 2, this.y + this.height / 2);
+                ctx.lineTo(dx, dy);
+            }
+            index++;
         }
-        ctx.stroke();
     }
     
-    rayCasting(wall){
+    rayCasting(wall, dx, dy){
         const x1 = wall.x1;
         const y1 = wall.y1;
         const x2 = wall.x2;
         const y2 = wall.y2;
         const x3 = this.x + this.width / 2;
         const y3 =  this.y + this.height / 2;
-        this.castingPoints = [];
-        this.rays.forEach(ray => {
-            const x4 = ray[0];
-            const y4 = ray[1];
-            let divider = (x1 - x2) * (y3 - y4) - (y1 - y2) * (x3 - x4);
-            let t = ((x1 - x3) * (y3 - y4) - (y1 - y3) * (x3 - x4)) / divider;
-            let u = -1 * ((x1 - x2) * (y1 - y3) - (y1 - y2)*(x1 - x3)) / divider;
-            if(t > 0 && t < 1 && u > 0){
-                const point = {
-                    x: x1 + t * (x2 - x1),
-                    y: y1 + t * (y2 - y1)
-                };
-                this.castingPoints.push(point);
-                console.log(point);
-            }
+        const x4 = dx;
+        const y4 = dy;
+        let divider = (x1 - x2) * (y3 - y4) - (y1 - y2) * (x3 - x4);
+        let t = ((x1 - x3) * (y3 - y4) - (y1 - y3) * (x3 - x4)) / divider;
+        let u = -1 * ((x1 - x2) * (y1 - y3) - (y1 - y2)*(x1 - x3)) / divider;
+        if(t > 0 && t < 1 && u > 0){
+            const point = {
+                x: x1 + t * (x2 - x1),
+                y: y1 + t * (y2 - y1)
+            };
+        this.castingPoints.push(point);
+        return point;
+        }
+    }
+
+    distance(x1, y1, x2, y2){
+        return Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+    }
+
+    rays(){
+        ctx.beginPath();
+        Object.keys(this.points).forEach(key => {
+            ctx.moveTo(this.x + this.width / 2, this.y + this.height / 2);
+            ctx.lineTo(this.points[key][0], this.points[key][1]);
         });
+        ctx.stroke();
     }
 
     draw(){
+        this.lengthPerIndexes = [...Array(this.viewAngle / this.angleStep + 1)].map((_, i) => Infinity); 
+        this.castingRays = {};
+        this.castingPoints = [];
+        this.points = {};
         ctx.fillStyle = "red";
         ctx.fillRect(this.x, this.y, this.width, this.height);
         ctx.fill();
@@ -80,6 +103,7 @@ class Player{
             }
         });
     }
+
     stop(keyCode){
         Object.keys(this.controller).forEach(key => {
             this.controller[keyCode].pressed = false;
